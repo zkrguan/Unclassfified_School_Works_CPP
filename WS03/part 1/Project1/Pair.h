ded825302dfff44e76e1f38f63dfd4910a060104
Zhaokai Guan <zguan25@myseneca.ca>
2022-05-28 22:54:34 -0400

    finished the headerfile

---
+++ WS03/part 1/Project1/Pair.h
@@ -0,0 +1,26 @@
+#ifndef SDDS_PAIR_H
+#define SDDS_PAIR_H
+#include <string>
+
+namespace sdds {
+	class Pair
+	{
+		std::string m_key{};
+		std::string m_value{};
+	public:
+		const std::string& getKey() { return m_key; }
+		const std::string& getValue() { return m_value; }
+		Pair(const std::string& key, const std::string& value) : m_key{ key }, m_value{ value }{};
+		
+		// TODO: Add here the missing prototypes for the members
+		//           that are necessary if this class is to be used
+		//           with the template classes described below.
+		//       Implement them in the Pair.cpp file.
+		
+		// operator overload for the ==
+		bool operator == (const Pair& src) const;
+
+	}
+}
+#endif // !SDDS_PAIR_H
+
